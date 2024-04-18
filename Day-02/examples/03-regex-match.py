d5a5a1f6e3064ff0fc11ea52acdaa337f370829a
Karthika M <123933555+karthik1234567899@users.noreply.github.com>
2024-04-18 17:58:54 +0530

    Update 03-regex-match.py

--- Day-02/examples/03-regex-match.py
+++ Day-02/examples/03-regex-match.py
@@ -1,4 +1,4 @@
-# This code used to get the word we provided in the first word in the line"
+# In this it just search for the word quick in the text, if the quick is io the 1st line of the text, then it pass otherwise it will show as no match"
 import re
 
 text = "The quick brown fox"
